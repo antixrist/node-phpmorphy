e7413ad3820d57fb131f30d09c983a6cb0ad7faf
antixrist <ya.antixrist@gmail.com>
2019-12-02 02:09:37 +0400

    remove unused babel's plugins

--- .babelrc.js
+++ .babelrc.js
@@ -10,17 +10,10 @@ module.exports = {
     ],
   ],
   plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-class-properties',
-    // '@babel/plugin-proposal-do-expressions',
-    // '@babel/plugin-proposal-optional-chaining',
-    // '@babel/plugin-proposal-nullish-coalescing-operator',
-    // '@babel/plugin-proposal-throw-expressions',
-    // '@babel/plugin-proposal-async-generator-functions',
-    // '@babel/plugin-proposal-private-methods',
-    // '@babel/plugin-proposal-decorators',
+    '@babel/plugin-proposal-object-rest-spread',
     [
       '@babel/plugin-transform-runtime',
       {
